c8bc067d1481d4595ac90db8125365f4af65a9a1
pjsacchet <psacchet@proton.me>
2024-06-17 19:05:42 -0400

    Add base python file for calling into DLL

---
+++ DCOM-Py/DCOM_Py_Main.py
@@ -0,0 +1,21 @@
+# Patrick Sacchet
+# Main functionality for user input and corresponding DLL calls
+
+import ctypes
+
+# Assume path to DLL is relative if user cloned our repo without structural changes 
+    # TODO: ask user for arch build and debug or release to get proper directory 
+def getDll():
+    dcom_functions = ctypes.cdll.LoadLibrary("..\\DCOM-Dll\\x64\\Debug\\DCOM-Dll.dll")
+    return dcom_functions 
+
+
+def main():
+    dcom_functions = getDll()
+    
+    print(dcom_functions.handleImplantlessStartup())
+
+
+
+if __name__ == '__main__':
+    main()
