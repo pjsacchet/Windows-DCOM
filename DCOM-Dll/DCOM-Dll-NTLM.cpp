10dfe00211e55ce07e49361f212b3a3ade3cc26f
pjsacchet <psacchet@proton.me>
2024-06-25 21:51:56 -0400

    Start work on Windows RM

--- DCOM-Dll/DCOM-Dll-NTLM.cpp
+++ DCOM-Dll/DCOM-Dll-NTLM.cpp
@@ -5,15 +5,17 @@
 #include "DCOM-Dll-NTLM.h"
 
 
-BOOL handleNTLMLogonPlainText(unsigned char* username, unsigned char* password, unsigned char* targetAddress, unsigned char* princName, unsigned char* domain)
+
+
+BOOL handleNTLMLogonPlainText(unsigned char* username, unsigned char* password, unsigned char* targetAddress, unsigned char* princName, unsigned char* domain, unsigned char* service)
 {
 	BOOL status = TRUE;
 	HRESULT result = S_OK;
 	CHAR msgBuf[DEFAULT_BUF_LEN];
+	DWORD count = 5;
 	COSERVERINFO serverInfo = { 0 };
 	COAUTHINFO authInfo = { 0 };
-	// Object we're using 
-	REFCLSID clsid = { 0 };
+	MULTI_QI results[5] = { 0 };
 
 
 
@@ -21,8 +23,14 @@ BOOL handleNTLMLogonPlainText(unsigned char* username, unsigned char* password, 
 	authInfo.dwAuthnSvc = RPC_C_AUTHN_WINNT;
 	authInfo.dwAuthzSvc = RPC_C_AUTHZ_NONE;
 
+	// Call for string conversion.... 
+
 	// Remotely create our object 
-	//status = CoCreateInstanceEx
+	status = CoCreateInstanceEx(CLSID_WSMAN, NULL, CLSCTX_REMOTE_SERVER, &serverInfo, count, results);
+	if (status != S_OK)
+	{
+		// sad
+	}
 	
 	
 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Windows-Implantless
 An access and survey tool that relies on traditional authentication protocols (NTLM, Kerberos etc.) to remotely authenticate with a target and utilize Distributed Component Object Model (DCOM) methods to remotely collect off target. 
+
+
+## How it Works
+We will split functionality in accordance to the various Windows services and their exposed COM  objects. This way, we will allow the user to determine which service they would like to use (given proper credentials) and implement the corresponding functionality available to us via COM/DCOM.
+
+## Services Offered
+Services include:
+- Windows Remote Management (WinRM) 
